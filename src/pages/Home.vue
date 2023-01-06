1b876526bbe748653a1cc8d1cb0ca6498b6e60f9
MohsenSami <mohsensami88@gmail.com>
2023-01-06 23:01:05 +0330

    axios added and show all products in home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,43 @@
 <template>
-  
+  <div class="container py-8">
+    <div class="grid grid-cols-4 gap-4">
+        <div class="flex flex-col gap-4 bg-white p-4" v-for="(product, index) in products" :key="index">
+            <div class="flex justify-center"><img width="250" :src="product.image" alt="{{product.title}"></div>
+            <h3 class="text-lg text-bold">{{product.title}}</h3>
+        </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import axios from "axios";
+const products = ref<any>([]);
+const loading = ref(true);
 
-}
+
+const getProducts = async() => {
+  await axios
+    .get('https://fakestoreapi.com/products')
+    .then(function (response) {
+      products.value = response.data;
+      loading.value = false;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+
+onMounted(async () => {
+      try {
+        getProducts();
+      } catch (error) {
+        
+      }
+    });
+
+
 </script>
 
 <style>
