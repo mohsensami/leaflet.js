dd4f76315e5c65aafc726ec9047ebe2a52c8f2e1
MohsenSami <mohsensami88@gmail.com>
2023-01-07 23:33:47 +0330

    Footer component added and navbar updated

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
   <div class="">
     <Header />
     <router-view></router-view>
-
+    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <style>
