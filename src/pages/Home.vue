9f385084ee2966995b1402598449b0d9df60c28c
MohsenSami <mohsensami88@gmail.com>
2023-01-07 23:06:39 +0330

    work on cart products in home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="container py-8">
-    <div class="grid grid-cols-4 gap-4">
-        <div class="flex flex-col gap-4 bg-white p-4" v-for="(product, index) in products" :key="index">
-            <div class="flex justify-center"><img width="250" :src="product.image" alt="{{product.title}"></div>
-            <h3 class="text-lg text-bold">{{product.title}}</h3>
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div class="flex flex-col justify-between gap-4 bg-white shadow-md" v-for="(product, index) in products" :key="index">
+            <div class="flex justify-center relative">
+              <div class="flex gap-1 items-center absolute left-0 text-sm p-1">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                </svg>
+                <span>{{product.rating.rate}}</span>
+              </div>
+              
+              <img class="pt-4" width="250" :src="product.image" :alt="product.title" />
+            </div>
+            <div class="flex flex-col gap-4 p-3">
+              <h3 class="text-lg text-bold relative">{{product.title.slice(0, 20)}}
+                <span class="absolute left-0 -top-6 text-sm">Count: {{product.rating.count}}</span>
+              </h3>
+              <div class="flex justify-between">
+                <span>Category: {{product.category}}</span>
+                <span>{{product.price}} $</span>
+              </div>
+            </div>
         </div>
     </div>
   </div>
